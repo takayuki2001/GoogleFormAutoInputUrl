47ffab2751814ef1d8cba1d7596d0ef6e5b05e4d
takayuki2001 <64475658+takayuki2001@users.noreply.github.com>
2022-06-25 12:37:25 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+   //ブラウザのコンソールとかで実行してください。
+    function f(){
+        //回答を保存した状態でDIVをみます。
+        //右下のフォームをクリアを押すと試せます。
+        let pram = [
+            {input:"入力値",id:"01234"},
+            {input:"入力値",id:"01234"},
+        ]
+
+        let baseUrl = "https://docs.google.com/forms/d/e/ID/formResponse?"
+
+        pram.map(v=>{
+            baseUrl = ${baseUrl}entry.${v.id}=${v.input}&
+        })
+
+        baseUrl = encodeURI(baseUrl)
+        console.log(baseUrl)
+    }
+    
+    f()
